6c96632af1e59cf787a949ff9b020115b269014d
edalastra <ctl_edalastra@uolinc.com>
2022-04-08 16:07:20 -0300

    Configuraçao middleware de erros

---
+++ notas-admin/src/shared/http/middlewares/error-middleware.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import { AppError } from '../../errors/AppError';
+
+export const errorMiddleware = (
+  error: Error,
+  request: Request,
+  response: Response,
+) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+};
--- notas-admin/src/shared/http/server.ts
+++ notas-admin/src/shared/http/server.ts
@@ -1,6 +1,13 @@
 import express from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+
 import env from '../env';
+import { errorMiddleware } from './middlewares';
 
 const app = express();
+
+app.use(cors);
+app.use(errorMiddleware);
 
 app.listen(env.PORT, () => 'Server running on ' + env.PORT);
