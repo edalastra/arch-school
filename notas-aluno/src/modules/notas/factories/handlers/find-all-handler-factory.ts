4be5a67635e162ccf9d1aaff27bf8fb75ff58885
edalastra <ctl_edalastra@uolinc.com>
2022-04-11 17:41:08 -0300

    ajuste teste e2e

--- notas-aluno/src/modules/notas/factories/handlers/find-all-handler-factory.ts
+++ notas-aluno/src/modules/notas/factories/handlers/find-all-handler-factory.ts
@@ -3,10 +3,12 @@ import { client } from '../../../../config/database';
 import { HttpHandlerInterface } from 'src/shared/types';
 import { FindAllNotaService } from '../../services';
 import { FindAllNotaHandler } from '../../infra/http/handlers/find-all-nota-handler';
+import { AlunosRepository } from '../../../alunos/infra/repositories';
 
 export const makeFindAllNotaHandler = (): HttpHandlerInterface => {
   const notasRepository = new NotaRepository(client);
-  const service = new FindAllNotaService(notasRepository);
+  const alunosRepository = new AlunosRepository(client);
+  const service = new FindAllNotaService(notasRepository, alunosRepository);
 
   return new FindAllNotaHandler(service);
 };
--- notas-aluno/tests/e2e/notas/handlers/find-all-nota-handler.test.ts
+++ notas-aluno/tests/e2e/notas/handlers/find-all-nota-handler.test.ts
@@ -5,18 +5,16 @@ import { createApp } from '../../../../src/config/app';
 describe('FindAllNotaHandler', function () {
   let app: Express.Application;
   beforeAll(async function () {
-    await client.query('CREATE TEMP TABLE aluno(LIKE aluno)');
-    await client.query('CREATE TEMP TABLE nota(LIKE nota)');
+    await client.query('DELETE FROM nota');
+    await client.query('DELETE FROM aluno');
 
     app = createApp();
   });
 
   beforeEach(async function () {
+    await client.query(`INSERT INTO aluno(id, nome) VALUES(1, 'test aluno')`);
     await client.query(
-      `INSERT INTO pg_temp.aluno(id, nome) VALUES(1, 'test aluno')`,
-    );
-    await client.query(
-      `INSERT INTO pg_temp.nota(id, aluno_id, valor) VALUES
+      `INSERT INTO nota(id, aluno_id, valor) VALUES
         (1, 1, 10.0),
         (2, 1, 8.0),
         (3, 1, 6.5)
@@ -25,28 +23,35 @@ describe('FindAllNotaHandler', function () {
   });
 
   afterEach(async function () {
-    await client.query('DROP TABLE IF EXISTS pg_temp.nota');
-    await client.query('DROP TABLE IF EXISTS pg_temp.aluno');
+    await client.query('DELETE FROM nota');
+    await client.query('DELETE FROM aluno');
   });
 
   describe('GET /v1/notas', () => {
     it('Should create a new nota', async () => {
-      const response = await request(app).get('/v1/notas');
+      const response = await request(app)
+        .get('/v1/notas')
+        .set('authorization', '1');
+
       expect(response.status).toBe(200);
 
       const expectResult = {
-        notas: [
-          {
-            aluno_id: 1,
-            nome: 'test aluno',
-            media: '8.16',
-            situacao: 'APROVADO',
-            notas: [10, 8, 6.5],
-          },
-        ],
+        notas: {
+          aluno_id: 1,
+          nome: 'test aluno',
+          media: '8.17',
+          situacao: 'APROVADO',
+          notas: [10, 8, 6.5],
+        },
       };
 
       expect(response.body).toEqual(expectResult);
+    });
+
+    it('Should return 400 when authorization is not valid', async () => {
+      const response = await request(app).get('/v1/notas');
+
+      expect(response.status).toBe(400);
     });
   });
 });
